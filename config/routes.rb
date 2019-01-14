7cf3b9bec3e618682bf72b655bbfedca1cada1c3
Harley Rayner <harleymteden@gmail.com>
2019-01-14 20:39:52 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :articles, only: %i[index show new create]
-=======
 
-  # resources :articles, only: %i[index show new create edit update]
->>>>>>> 0e146e43b29034a209eeeea09c261dd311e5a7b3
+  resources :articles, only: %i[index show new create]
 
   get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
 
   patch 'articles/:id', to: 'articles#update'
-<<<<<<< HEAD
-=======
-  
->>>>>>> 0e146e43b29034a209eeeea09c261dd311e5a7b3
+
 end
